80528fee493263801e4a91cfb582cbfe7cbdd492
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-01-02 12:30:23 +0530

    Material UI setup done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 
 const Home = () => {
     return (
         <>
-            
+            <Button variant="contained" color="success">
+                Success
+            </Button>
         </>
     )
 }
